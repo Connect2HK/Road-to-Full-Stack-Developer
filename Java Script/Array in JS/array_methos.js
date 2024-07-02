d4eb2a5b560913500b71d43d176fd6ba996138ef
Connect2HK <hk2567840@gmail.com>
2024-07-02 12:05:33 +0530

    update commit

---
+++ Java Script/Array in JS/array_methos.js
@@ -0,0 +1,6 @@
+// slice method used to give contegeous segment of the array 
+
+let arr = [1,2,3,4,5,6];
+consolelog(arr.slice(2,5)); // this slice method copied the value of the array means it return the copied array value with new memory location
+
+// we can use it as well arr[0:3];
