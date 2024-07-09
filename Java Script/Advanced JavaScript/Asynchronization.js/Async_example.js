a5abbc3283a27b145d7079599b6ae61f74351e98
Connect2HK <hk2567840@gmail.com>
2024-07-09 15:09:31 +0530

    updated

--- Java Script/Advanced JavaScript/Asynchronization.js/Async_example.js
+++ Java Script/Advanced JavaScript/Asynchronization.js/Async_example.js
@@ -1,5 +1,8 @@
 //Synchronization in JS executes line by line codes and if any time taken task occures then it block the entire code till the execution of time taken taken then move to execute next upcoming task
-
+// Async is diff 
+// Async is a function that is executed in parallel with other tasks and it does not block the entire
+// code execution. It is used to execute a task in the background and then execute the next task
+// but if native js code is available along with Runtime envirment set of functionality that enable multithreading  or say async in js like setTimeout, getElementbyID etc then first the native js function/code will execute first 
 console.log('Sync is about to start');
 console.log('1');
 setTimeout(() => {
