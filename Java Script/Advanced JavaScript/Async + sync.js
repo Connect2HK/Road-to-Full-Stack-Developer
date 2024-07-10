785e0cdc74a3d648c55ae1958f2741b5212c9f32
Connect2HK <hk2567840@gmail.com>
2024-07-10 17:59:32 +0530

    added all

--- Java Script/Advanced JavaScript/Async + sync.js
+++ Java Script/Advanced JavaScript/Async + sync.js
@@ -9,11 +9,16 @@ console.log('Sync finished');
 console.log('Async is starting......Entire code will be blocked till the for loop not execute succefully');
 setTimeout(() => {
         console.log('Timer 1: We are checking it and executable this is in last.... Welcome to Async');
-}, 500);
+}, 5000);
 setTimeout(() => {
     console.log('Timer 2');
 }, 0);
-
+for(let i=0;i<10000000000;i++){
+    // body of the executable code
+}
+setTimeout(() => {
+    console.log('Timer 3 : it will be execute after timer 1 coz it will be execute after blocking fn for loop till the execution of timer 1 will be completed in Runtime envirnment and then callstack queue will enque timer1 then push it into callstack due to having native js fn ');
+}, 100);
 console.log('Aysnc will done after 500 milisec');
 console.log('Once the all native JS code in main thread will execute then async will be execute');
 console.log('Async finished');
