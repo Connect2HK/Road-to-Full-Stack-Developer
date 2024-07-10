642036d2ab825fbe3ef0486baa2c57458c4ade75
Connect2HK <hk2567840@gmail.com>
2024-07-10 16:56:48 +0530

    added more

---
+++ Java Script/String in JS/String Methods/typeconversion.js
@@ -0,0 +1,12 @@
+// string to number 
+/**
+ * parseInt
+ */
+let str = 'Type convertion';
+let result = parseInt(str);
+console.log(result);
+console.log(typeof(result));
+
+let strToNumber = '10';
+let output = parseInt(strToNumber);
+console.log(output);
