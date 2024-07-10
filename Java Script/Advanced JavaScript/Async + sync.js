5b436ef91af38df960968260a482282036620332
Connect2HK <hk2567840@gmail.com>
2024-07-10 17:10:01 +0530

    updated code

--- Java Script/Advanced JavaScript/Async + sync.js
+++ Java Script/Advanced JavaScript/Async + sync.js
@@ -6,5 +6,10 @@ for(let i=0;i<10000000000;i++){
     // body of the executable code
 }
 console.log('Sync finished');
-console.log('Async is starting......');
-setTimeout()
+console.log('Async is starting......Entire code will be blocked till the for loop not execute succefully');
+setTimeout(() => {
+        console.log('We are checking it and executable this is in last.... Welcome to Async');
+}, 500);
+console.log('Aysnc will done after 500 milisec');
+console.log('Once the all native JS code in main thread will execute then async will be execute');
+console.log('Async finished');
