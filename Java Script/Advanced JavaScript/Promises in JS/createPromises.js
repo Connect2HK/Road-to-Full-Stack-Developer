2422672ad57e004320e58193cb5ecd75972e6687
Connect2HK <hk2567840@gmail.com>
2024-07-11 21:43:48 +0530

    added codess

--- Java Script/Advanced JavaScript/Promises in JS/createPromises.js
+++ Java Script/Advanced JavaScript/Promises in JS/createPromises.js
@@ -1,3 +1,15 @@
 //Will learn how to use create Promises and why Its need
 //Promise is a object that represents the eventual completion or failure of an asynchronous operation
+// promised are used tackles the flaws/issues of callback functions like: CallBack hell -> issue with readability of code
+//2. Inversion of control: means giving access to third party like envirnment functions or taking funciton from runtime env of browser
+//properties of promises:  1. status -> pendin(when we initailized promises it always in pending state) -> fullfilled or rejected and once it completed state cant change the status fromrejected or fulfilled whether the value can be change
+// value of promise can not change without state change i.e. if promise is in pending state then value is always undefined
+// 2. value 3. Onfullfillment 4. Onrejection 
+// we can use it like asyc n sync call back function // it is special kind of object having placeholder for future task like callback
+// 1. initailize promise 2. resolve promise 3. reject promise 4
+// 5. then 6. catch 7. finally 8. all 9.
+// 10. race 11. allSettled 12. any 13. all
+// 14. Promise.race 15. Promise.allSettled 16. Promise.any
+// 17. Promise.all 18. Promise.resolve 19. Promise.reject 20. Promise
+
 
---
+++ Java Script/String in JS/String Methods/replaceAll_method.js
@@ -0,0 +1,2 @@
+let str = 'Replacing the String Value';
+console.log(str.replace('String','The new string value is 1'));
