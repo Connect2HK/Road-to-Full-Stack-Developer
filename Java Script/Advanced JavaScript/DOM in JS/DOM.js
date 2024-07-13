2131acc5c24b08f0e7d8b5fcfb86594731290795
Connect2HK <hk2567840@gmail.com>
2024-07-13 21:00:21 +0530

    here one

--- Java Script/Advanced JavaScript/DOM in JS/DOM.js
+++ Java Script/Advanced JavaScript/DOM in JS/DOM.js
@@ -1,1 +1,5 @@
 // DOM stand for Data Object model that use to parse HTML CSS JS into tree like structures 
+// DOM is a tree like structure that is used to represent the HTML document
+// DOM is a W3C (World Wide Web Consortium) standard
+// DOM is a programming interface for HTML and XML documents
+
