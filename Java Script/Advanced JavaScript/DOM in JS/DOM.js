d90935578865333401c8fd3b1796d9f7270bed31
Connect2HK <hk2567840@gmail.com>
2024-07-13 21:00:51 +0530

    here two

--- Java Script/Advanced JavaScript/DOM in JS/DOM.js
+++ Java Script/Advanced JavaScript/DOM in JS/DOM.js
@@ -2,4 +2,5 @@
 // DOM is a tree like structure that is used to represent the HTML document
 // DOM is a W3C (World Wide Web Consortium) standard
 // DOM is a programming interface for HTML and XML documents
+// DOM tree is a logical tree of HTML elements and text nodes
 
