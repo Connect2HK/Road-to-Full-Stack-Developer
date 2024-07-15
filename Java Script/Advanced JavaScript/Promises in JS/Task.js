0c2f3125aee91927d2a495a7c638e3198d0c11e9
Connect2HK <hk2567840@gmail.com>
2024-07-15 22:42:49 +0530

    adddd

--- Java Script/Advanced JavaScript/Promises in JS/Task.js
+++ Java Script/Advanced JavaScript/Promises in JS/Task.js
@@ -22,7 +22,7 @@ function uploadFile(fileName, newUrl, cb) {
 
 function downloader(url, cb) {  
     setTimeout(() => {
-        cb(url);             // here cb(url) used for download data of 
+        cb(url);             // here cb(url) used for download data of url 
                             
     }, 4000)
 }
