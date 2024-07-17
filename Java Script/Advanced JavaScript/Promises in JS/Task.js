2f8bc26c609fe7daef7d188e3fe090734e31ebda
Connect2HK <hk2567840@gmail.com>
2024-07-17 11:32:09 +0530

    codess

--- Java Script/Advanced JavaScript/Promises in JS/Task.js
+++ Java Script/Advanced JavaScript/Promises in JS/Task.js
@@ -37,7 +37,7 @@ function uploadFile(fileName, newUrl, cb) {
         cb(fileName);
     }, 3000)
 }
-function downloadFile(url, cb) {                   // here it use 
+function downloadFile(url, cb) {                   // here it use as function 
     downloader(url, (data) => {
         writeFile(data, (data) => {             
             uploadFile(data, (data) => {
