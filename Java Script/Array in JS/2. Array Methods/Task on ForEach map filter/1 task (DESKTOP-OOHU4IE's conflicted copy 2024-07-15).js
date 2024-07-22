d00708b32144d4857eca106d48dd17f1a4b5b2b1
Connect2HK <hk2567840@gmail.com>
2024-07-22 20:30:00 +0530

    hiii

--- Java Script/Array in JS/2. Array Methods/Task on ForEach map filter/1 task (DESKTOP-OOHU4IE's conflicted copy 2024-07-15).js
+++ Java Script/Array in JS/2. Array Methods/Task on ForEach map filter/1 task (DESKTOP-OOHU4IE's conflicted copy 2024-07-15).js
@@ -16,7 +16,7 @@ let newArr =[
 const modifiedResult = newArr.filter(elements => elements.price === 10000).map(element => element.name);
 console.log(modifiedResult);
 
-// this is used when 
+// this is used when we 
 
 
 // prob 3
